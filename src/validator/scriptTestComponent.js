15d1c0a13aaf0eb4e0d6c958b3b7d979842d3f34
eladzlot <eladzlot@gmail.com>
2015-12-03 15:17:39 +0200

    chore(git): don't diff dist

--- src/validator/scriptTestComponent.js
+++ src/validator/scriptTestComponent.js
@@ -68,34 +68,34 @@ var scriptTestComponent = {
 };
 
 
-	function stringify(value, pretty) {
-		if (value == null) { // null || undefined
-			return '<i class="text-muted">undefined</i>';
-		}
-		if (value === '') {
-			return '<i class="text-muted">an empty string</i>';
-		}
-
-		switch (typeof value) {
-			case 'string':
-				break;
-			case 'number':
-				value = '' + value;
-				break;
-			case 'object':
-				// display the error message not the full thing...
-				if (value instanceof Error){
-					value = value.message;
-					break;
-				}
-			/* fall through */
-			default:
-				// @TODO: implement this: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
-				value = syntaxHighlight(JSON.stringify(value));
-		}
-
-		return value;
+function stringify(value, pretty) {
+	if (value == null) { // null || undefined
+		return '<i class="text-muted">undefined</i>';
 	}
+	if (value === '') {
+		return '<i class="text-muted">an empty string</i>';
+	}
+
+	switch (typeof value) {
+		case 'string':
+			break;
+		case 'number':
+			value = '' + value;
+			break;
+		case 'object':
+			// display the error message not the full thing...
+			if (value instanceof Error){
+				value = value.message;
+				break;
+			}
+		/* fall through */
+		default:
+			// @TODO: implement this: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+			value = syntaxHighlight(JSON.stringify(value));
+	}
+
+	return value;
+}
 
 
 function syntaxHighlight(json) {
