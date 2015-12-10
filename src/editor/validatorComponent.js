17205b4a6dd6dd05446442da9e37fb28929a8ceb
eladzlot <eladzlot@gmail.com>
2015-12-10 15:06:20 +0200

    refactor(editor): remove console.log

--- src/editor/validatorComponent.js
+++ src/editor/validatorComponent.js
@@ -97,7 +97,6 @@ function stringify(value, pretty) {
 
 
 function syntaxHighlight(json) {
-	console.log(json)
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
