cbbda8855ede937bba72f23f8cbb72dcf3d0a2f5
eladzlot <eladzlot@gmail.com>
2015-12-13 16:36:00 +0200

    chore(contextMenu): still playing around

--- src/contextMenuComponent.js
+++ src/contextMenuComponent.js
@@ -2,23 +2,47 @@ import classNames from './classNames';
 export default contextMenuComponent;
 
 let contextMenuComponent = {
+	show: m.prop(false),
+	style: m.prop({}),
+
 	controller: args => {
 
 	},
 
 	view: (ctrl, args) => {
-		return m('.context-menu', args.menu.map(menuNode));
+		return m(
+			'.context-menu',
+			{
+				class: classNames({'show-menu': contextMenuComponent.show()}),
+				style: contextMenuComponent.style(),
+				onclick: e => e.stopPropagation()
+			},
+			args.menu.map(menuNode)
+		);
+	},
+
+	oncontextmenu: e => {
+		contextMenuComponent.show(true);
+		contextMenuComponent.style({left:e.pageX, top:e.pageY});
+
+		document.addEventListener('click', onClick, false);
+		function onClick(){
+			contextMenuComponent.show(false);
+			document.removeEventListener('click', onClick);
+		}
 	}
 };
 
 let menuNode = node => {
-	return m('.context-menu-item', {class: classNames({disabled: node.disabled})}, [
-		m('button.context-menu-btn',{onclick:node.action}, [
-			m('i.fa', {class:node.icon}),
-			m('span.context-menu-text', node.text),
-			node.menu ? m('.context-menu', node.menu.map(menuNode)) : ''
-		])
-	]);
+	return node.separator
+		? m('.context-menu-separator')
+		: m('.context-menu-item', {class: classNames({disabled: node.disabled})}, [
+			m('button.context-menu-btn',{onclick:node.action}, [
+				m('i.fa', {class:node.icon}),
+				m('span.context-menu-text', node.text),
+				node.menu ? m('.context-menu', node.menu.map(menuNode)) : ''
+			])
+		]);
 };
 
 
