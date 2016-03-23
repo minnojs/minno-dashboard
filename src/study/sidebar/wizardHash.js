d9b3dc43226187ceb972984197fff57892444116
baranan <baranan@gmail.com>
2016-03-23 12:06:58 -0400

    Update wizardHash.js

--- src/study/sidebar/wizardHash.js
+++ src/study/sidebar/wizardHash.js
@@ -4,16 +4,32 @@ let hash = {};
 
 hash.piPlayer = {
 	'Empty': '/implicit/user/yba/wizards/emptyPIP.js',
-	'IAT extenssion': '/implicit/user/yba/wizards/iat.js',
-	'Attitude induction': '/implicit/user/yba/wizards/attitude.js',
-	'Evaluative Conditioning': '/implicit/user/yba/wizards/conditioning.js',
-	'Mobile IAT': '/implicit/user/yba/wizards/mobile.js',
-	'Sorting Task': '/implicit/user/yba/wizards/sorting.js'
+	'IAT (images)': '/implicit/user/yba/wizards/iatimages.js',
+	'IAT (words)': '/implicit/user/yba/wizards/iatwords.js',
+	'IAT (modify attributes)': '/implicit/user/yba/wizards/iatatt.js',
+	'IAT (all the parameters)': '/implicit/user/yba/wizards/iatall.js',
+	'Mobile IAT': '/implicit/user/yba/wizards/iatmobile.js',
+	'ST-IAT': '/implicit/user/yba/wizards/stiatsimple.js',
+	'ST-IAT (all parameters)': '/implicit/user/yba/wizards/stiatall.js',
+	'AMP (with words as primes)': '/implicit/user/yba/wizards/ampwords.js',
+	'AMP (all parameters)': '/implicit/user/yba/wizards/ampall.js',
+	'Brief-IAT': '/implicit/user/yba/wizards/batsimple.js',
+	'Brief-IAT (all parameters)': '/implicit/user/yba/wizards/batall.js',
+	'Attitude induction (behaviors)': '/implicit/user/yba/wizards/attitude.js',
+	'Evaluative Conditioning': '/implicit/user/yba/wizards/ec.js',
+	'Simple sorting task': '/implicit/user/yba/wizards/sorting.js'
 };
 
 hash.piQuest = {
 	'Empty': '/implicit/user/yba/wizards/emptyQuest.js',
-	'Rating Questionnaire': '/implicit/user/yba/wizards/rating.js'
+	'Rating Questionnaire': '/implicit/user/yba/wizards/rating.js',
+	'Rating Questionnaire (with images)': '/implicit/user/yba/wizards/ratingimages.js'
+};
+
+hash.piMessage = {
+	'Consent (Yoav lab)': '/implicit/user/yba/wizards/consent.jst',
+	'Intro': '/implicit/user/yba/wizards/intro.jst',
+	'Debriefing': '/implicit/user/yba/wizards/debriefing.jst'
 };
 
 hash.piManager = {
