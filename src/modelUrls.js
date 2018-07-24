dab07378cca571d9e4eb801d7570df3b2645c94a
ronenno1 <ronen.no1@gmail.com>
2018-07-24 10:30:02 +0300

    fix(urls): fix url for server

--- src/modelUrls.js
+++ src/modelUrls.js
@@ -1,8 +1,12 @@
 /**/
-const urlPrefix = 'http://app-prod-03.implicit.harvard.edu/openserver'; // first pathname section with slashes
+// const urlPrefix = 'http://app-prod-03.implicit.harvard.edu/openserver'; // first pathname section with slashes
 
 // const urlPrefix = window.location.origin; // first pathname section with slashes
 
+const urlPrefix = '..';//location.pathname.match(/^(?=\/)(.+?\/|$)/)[1]; // first pathname section with slashes
+
+
+// console.log(location.href);
 export const baseUrl            = `${urlPrefix}`;
 export const studyUrl           = `${urlPrefix}/studies`;
 export const launchUrl          = `${urlPrefix}/launch`;
